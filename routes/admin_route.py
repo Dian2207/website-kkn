d649b40d7abfb452e7d5d4c9ad51c9b8a65db70a
Maheswari Dian P <maheswaridian@icloud.com>
2026-07-25 11:36:49 +0700

    admin

---
+++ routes/admin_route.py
@@ -0,0 +1,15 @@
+from flask import Blueprint, render_template
+
+from models import Infographics
+
+admin_bp = Blueprint(
+    "admin_bp",
+    __name__,
+    static_folder="../static/static_admin",
+    static_url_path="/static_admin",
+    template_folder="../templates/admin",
+)
+
+@admin_bp.route("/index-admin")
+def indexAdmin():
+    return render_template("user/index.html")
