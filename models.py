909d230fc527c19d3f832e41bd7afd23708953bc
Maheswari Dian P <maheswaridian@icloud.com>
2026-07-25 14:34:33 +0700

    dinamis dashboard

--- models.py
+++ models.py
@@ -8,3 +8,41 @@ class Infographics(db.Model):
 
     total_population = db.Column(db.Integer, nullable=False)
     total_family = db.Column(db.Integer, nullable=False)
+
+
+# ---------- NEWS ----------
+class News(db.Model):
+    __tablename__ = "news"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(255), nullable=False)
+    slug = db.Column(db.String(255), nullable=False, unique=True)
+
+    content = db.Column(db.Text, nullable=False)
+
+    thumbnail = db.Column(db.String(255), nullable=True)
+
+    published_at = db.Column(db.DateTime, nullable=False)
+
+    created_at = db.Column(db.DateTime)
+
+    updated_at = db.Column(db.DateTime)
+
+# ---------- ANNOUNCEMENT ----------
+class Announcement(db.Model):
+    __tablename__ = "announcement"
+
+    id = db.Column(db.Integer, primary_key=True)
+
+    title = db.Column(db.String(255), nullable=False)
+
+    description = db.Column(db.Text)
+
+    event_date = db.Column(db.DateTime)
+
+    location = db.Column(db.String(255))
+
+    created_at = db.Column(db.DateTime)
+
+    type = db.Column(db.String(100))
--- routes/route.py
+++ routes/route.py
@@ -1,24 +1,37 @@
 from flask import Blueprint, render_template
-from models import Infographics
-
+from models import Infographics, News, Announcement
 
 main = Blueprint("main", __name__)
-
 
 # ==========================
 # HALAMAN UTAMA
 # ==========================
-
-@main.route("/")
+@main.route('/')
 def index():
 
     infographic = Infographics.query.first()
 
-    return render_template(
-        "user/index.html",
-        infographic=infographic
+    news = (
+        News.query
+        .order_by(News.published_at.desc())
+        .limit(2)
+        .all()
     )
 
+    announcements = (
+        Announcement.query
+        .filter_by(type="Pengumuman")
+        .order_by(Announcement.created_at.desc())
+        .limit(5)
+        .all()
+    )
+
+    return render_template(
+        "user/index.html",
+        infographic=infographic,
+        news=news,
+        announcements=announcements
+    )
 
 # ==========================
 # PROFIL DESA
